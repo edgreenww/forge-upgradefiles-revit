96bb94a9b1c2f5451d68601d4a5f9d5bf36ff5c7
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-05-28 17:41:41 +0100

    disable middleware for test

--- routes/unzip.js
+++ routes/unzip.js
@@ -5,28 +5,28 @@ let router = express.Router();
 ///////////////////////////////////////////////////////////////////////
 /// Middleware for obtaining a token for each request.
 ///////////////////////////////////////////////////////////////////////
-router.use(async (req, res, next) => {
-    const oauth = new OAuth(req.session);
-    let credentials = await oauth.getInternalToken();
+// router.use(async (req, res, next) => {
+//     const oauth = new OAuth(req.session);
+//     let credentials = await oauth.getInternalToken();
 
-    let oauth_client = oauth.getClient();
+//     let oauth_client = oauth.getClient();
 
-    let incomingCreds = {
-        "client_id" : req.body.client_id,
-        "client_secret" : req.body.client_secret,
+//     let incomingCreds = {
+//         "client_id" : req.body.client_id,
+//         "client_secret" : req.body.client_secret,
 
-    }
-    console.log('credentials', credentials)
-    if (incomingCreds.client_id){
-        console.log("Using incoming credentials...")
-        credentials = incomingCreds
-    }
-    // let oauth_client2 = oauth.getClientOurWay(req.scopes, incomingCreds);
-    console.log('oauth_client', oauth_client)
-    req.oauth_client = oauth_client;
-    req.oauth_token = credentials;
-    next();
-});
+//     }
+//     console.log('credentials', credentials)
+//     if (incomingCreds.client_id){
+//         console.log("Using incoming credentials...")
+//         credentials = incomingCreds
+//     }
+//     // let oauth_client2 = oauth.getClientOurWay(req.scopes, incomingCreds);
+//     console.log('oauth_client', oauth_client)
+//     req.oauth_client = oauth_client;
+//     req.oauth_token = credentials;
+//     next();
+// });
 
 
 ///////////////////////////////////////////////////////////////////////
