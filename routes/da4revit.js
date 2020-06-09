47bedb89ccc38c61f2b75f747c9539f84e34cc2b
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-06-09 21:32:25 +0100

    debugging file download

--- routes/da4revit.js
+++ routes/da4revit.js
@@ -544,9 +544,9 @@ router.post('/da4revit/v1/upgrader/files/unzip', async (req, res, next) => {
     console.log('Attempting to stream download from URL: ', url)
     // download(url, downloadFilePath, token, extractFiles, req)
 
-    download(url, downloadFilePath, token, extractFiles, req, extract=true)
+    await download(url, downloadFilePath, token, null, req, extract=false)
 
-    // console.log("file downloaded" )
+    console.log("file downloaded" )
     // const hostId = folder.body.data.id
     // await createStorage(req, hostId)
 
