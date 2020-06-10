8775e0c9710c10c4eee5f533f6a7abefef0f217e
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-06-10 07:08:24 +0100

    betterCreateStorage

--- routes/da4revit.js
+++ routes/da4revit.js
@@ -231,6 +231,48 @@ const extractFiles =  (req) => {
     });
 }
 
+const betterCreateStorage = async (req, fileName) => {
+    console.log('In betterCreateStorage...')
+    const projectId = req.body.project_id
+    const folder = req.folder
+    const url = `https://developer.api.autodesk.com/data/v1/projects/${projectId}/storage`
+
+    const x_user_id = ''
+    const token = req.body.oauth_token
+    const headers = {
+        // "x-user-id": x_user_id, 
+        "Content-Type": "application/vnd.api+json", 
+        "Authorization": `Bearer ${token}`
+    }
+    const name = fileName // req.body.fileItemName
+    const hostType = 'folders'
+    const hostId = folder.body.data.id
+    const data = {
+        "jsonapi": {"version": "1.0"},
+        "data": {
+            "type": "objects",
+            "attributes": {"name": name},
+            "relationships": {
+                "target": {"data": {"type": hostType, "id": hostId}}
+            },
+        },
+    }
+
+    const requestParams = {
+        headers: headers,
+        uri: url,
+        method: 'POST',
+        body: data
+    }
+
+    console.log('Ready to create storage...')
+    const storageResult = request(requestParams, function (error, response, body) {
+        console.log('Response ', response)
+        console.log('body: ', body)
+
+        // unzipCallback(filePath, uploadUnzippedFile, req)
+    })
+}
 
 
 const createStorage = async (req, unzippedFilePath) => {
@@ -284,29 +326,30 @@ const createStorage = async (req, unzippedFilePath) => {
     const folder = req.folder
 
     console.log(`Creating storage based on ${fileItemName} `)
-    try{
+    // try{
 
-        const storageInfo = await getNewCreatedStorageInfo(
-            projectId, 
-            folder.body.data.id, 
-            fileName, 
-            req.oauth_client, 
-            incoming_oauth_token
-            );
-        if (storageInfo === null ) {
-            console.log('failed to create the storage');
-            // res.status(500).end('failed to create the storage');
-            return;
-        }
-        const outputUrl = storageInfo.StorageUrl;
-        console.log('Creating storage..  OK')
-    } catch (err) {
-        console.log('Error creating storage.. ')
-        console.log(err)
-        console.log("err.statusBody")
-        console.log(JSON.stringify(err.statusBody, null, 4))
-    }
+    //     const storageInfo = await getNewCreatedStorageInfo(
+    //         projectId, 
+    //         folder.body.data.id, 
+    //         fileName, 
+    //         req.oauth_client, 
+    //         incoming_oauth_token
+    //         );
+    //     if (storageInfo === null ) {
+    //         console.log('failed to create the storage');
+    //         // res.status(500).end('failed to create the storage');
+    //         return;
+    //     }
+    //     const outputUrl = storageInfo.StorageUrl;
+    //     console.log('Creating storage..  OK')
+    // } catch (err) {
+    //     console.log('Error creating storage.. ')
+    //     console.log(err)
+    //     console.log("err.statusBody")
+    //     console.log(JSON.stringify(err.statusBody, null, 4))
+    // }
 
+    await betterCreateStorage(req, fileName )
 
     
     // const url = `https://developer.api.autodesk.com/data/v1/projects/${projectId}/storage`
