be2cf6bae2f697e48604ed3702debd1f674a24e0
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-06-11 14:59:34 +0100

    Fix: itemId not defined

--- routes/da4revit.js
+++ routes/da4revit.js
@@ -514,7 +514,7 @@ const uploadFile = async (req, data) => {
     uploadPromise.then( async (result) => {
         console.log('Upload promise resolved'.brightGreen.bold)
         console.log(JSON.stringify(result, null, "----"))
-        const version =  await createVersion(itemId, storageId, fileName)
+        const version =  await createVersion(req.body.itemId, storageId, fileName)
         console.log('Version created'.green.bold)
         console.log(JSON.stringify(version, null, "----"))
         
