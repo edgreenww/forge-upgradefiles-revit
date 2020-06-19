bbd31c4feb3c40889ef5950003bec31b7bce2c8f
edgreenww <51371812+edgreenww@users.noreply.github.com>
2020-06-19 13:37:39 +0100

    back to EdG method

--- routes/common/unzip.js
+++ routes/common/unzip.js
@@ -600,7 +600,7 @@ const uploadObjectChunked = (token, bucketKey, objectKey,
                 file.size, (chunkIdx + 1) * chunkSize) - 1
 
             const range = `bytes ${start}-${end}/${file.size}`
-
+            console.log(range)
             const length = end - start + 1
 
             const readStream =
@@ -726,89 +726,91 @@ const uploadFile = async (req, data) => {
 
     // https://forge.autodesk.com/blog/nailing-large-files-uploads-forge-resumable-api
 
-    const opts = {}
+    // const opts = {}
 
-    const response = await uploadObjectChunked(
-        credentials,
-        bucketKey,
-        objectName,
-        body,
-        opts
-    )
+    // const response = await uploadObjectChunked(
+    //     credentials,
+    //     bucketKey,
+    //     objectName,
+    //     body,
+    //     opts
+    // )
 
-    console.log('uploadChunk response', response.body)
+    // console.log('uploadChunk response', response.body)
 
-    const version =  await createVersion(req)
-    console.log('Version created'.green.bold)
+    // const version =  await createVersion(req)
+    // console.log('Version created'.green.bold)
 
     /// First attempt
 
-    // let sessionId = "-12345"
+    let sessionId = "-12345"
 
-    // let promises = []
-    // const chunkSize = 4999999 // 5MB in bytes
-    // let start = 0
-    // let end = start + chunkSize
-    // console.log("Chunk upload...")
-    // while (end < contentLength-1){
-    //     end = start + chunkSize - 1
+    let promises = []
+    const chunkSize = 4999999 // 5MB in bytes
+    let start = 0
+    let end = start + chunkSize
+    console.log("Chunk upload...")
+    while (end < contentLength-1){
+        end = start + chunkSize - 1
 
         
         
-    //     if ( end > contentLength-1){
-    //         end = contentLength-1
-    //     }
+        if ( end > contentLength-1){
+            end = contentLength-1
+        }
 
-    //     let contentRange = `bytes ${start}-${end}/${contentLength}`
-    //     console.log('contentRange', contentRange)
+        let contentRange = `bytes ${start}-${end}/${contentLength}`
+        console.log('contentRange', contentRange)
 
 
-    //     let readStream = fs.createReadStream(filePath, {start, end})
+        let readStream = fs.createReadStream(filePath, {start, end})
         
-    //     let chunkUploadPromise = objects.uploadChunk(
-    //         bucketKey,
-    //         objectName,
-    //         contentLength,
-    //         contentRange,
-    //         sessionId,
-    //         readStream, // body.slice(start, end),
-    //         {},
-    //         oauth2client,
-    //         credentials
-    //         )
-    //         console.log('chunkUploadPromise', chunkUploadPromise)
+        let chunkUploadPromise = objects.uploadChunk(
+            bucketKey,
+            objectName,
+            contentLength,
+            contentRange,
+            sessionId,
+            readStream, // body.slice(start, end),
+            {},
+            oauth2client,
+            credentials
+            )
+            console.log('chunkUploadPromise', chunkUploadPromise)
 
-    //         chunkUploadPromise.then((result) => {
-    //             console.log('chunkUploadPromise - resolved', result)
-    //         }, (result) => {
-    //             console.log('chunkUploadPromise - rejected', result)
-    //         })
+            // chunkUploadPromise.then((result) => {
+            //     console.log('chunkUploadPromise - resolved', result)
+            // }, (result) => {
+            //     console.log('chunkUploadPromise - rejected', result)
+            // })
 
-    //         promises.push(chunkUploadPromise) 
+            promises.push(chunkUploadPromise) 
             
-    //         if (end < contentLength){
-    //             start += chunkSize
-    //         }
-    //     }
-    // const chunksUploadPromises = Promise.all(promises)   
+            if (end < contentLength){
+                start += chunkSize
+            }
+        }
 
-    // console.log('chunksUploadPromises', chunksUploadPromises)
+        
+    const chunksUploadPromises = Promise.all(promises)   
+
+    console.log('chunksUploadPromises', chunksUploadPromises)
     
-    // let uploadPromise = chunksUploadPromises
+    let uploadPromise = chunksUploadPromises
 
-    // // uploadPromise = promises[0]
+    // uploadPromise = promises[0]
 
-    // uploadPromise.then( async (result) => {
-    //     console.log('Upload promise resolved'.brightGreen.bold)
-    //     console.log(JSON.stringify(result, null, "----"))
+    uploadPromise.then( async (result) => {
+        console.log('Upload promise resolved'.brightGreen.bold)
+        console.log(JSON.stringify(result, null, "----"))
 
-    //     const version =  await createVersion(req)
-    //     console.log('Version created'.green.bold)
-    //     // console.log(JSON.stringify(version, null, "----"))
-    // }, function(result){
-    //     console.log("Upload promise rejected".red.bold)
-    //     console.log(JSON.stringify(result, null, "----"))
-    // })
+        const version =  await createVersion(req)
+        console.log('Version created'.green.bold)
+        // console.log(JSON.stringify(version, null, "----"))
+    }, function(result){
+        console.log("Upload promise rejected".red.bold)
+        console.log(JSON.stringify(result, null, "----"))
+    })
 
 }
 
