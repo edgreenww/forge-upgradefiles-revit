9c08300d1a881c9d12c646c405327e25cca3b260
Luis Felipe Paris <lfparis@gmail.com>
2020-06-23 16:27:59 +0100

    added snoop print into slicedArrayBuffer

--- routes/common/unzip.js
+++ routes/common/unzip.js
@@ -1200,9 +1200,9 @@ const slicedArrayBuffer = (fileBuffer, start, end) => {
     let ab = new ArrayBuffer(end - start + 1);
     let view = new Uint8Array(ab);
     for (let i = start; i < end + 1; i++) {
-        if (i % 1000000 === 0) {
-            console.log(i, fileBuffer[i])
-        }
+        // if (i % 1000000 === 0) {
+        //     console.log(i, fileBuffer[i])
+        // }
         view[i] = fileBuffer[i];
     }
     return ab;
